c16dc238fdaee07cbff6aa3838eadaa4b79caffc
1prasadjr <prasadkanchar2004@gmail.com>
2025-07-17 19:04:57 +0530

    Restrict CORS to localhost and deployed frontend

--- server/server.js
+++ server/server.js
@@ -14,7 +14,14 @@ const app = express();
 
 // Middleware
 app.use(compression());
-app.use(cors());
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://prepstation-eight.vercel.app"
+      ]
+    })
+  );
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
