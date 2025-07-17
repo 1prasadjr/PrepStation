8b9eaecee6b7ba85773a5f23263465cede1e3823
1prasadjr <prasadkanchar2004@gmail.com>
2025-07-17 19:28:24 +0530

    To fetch Api calls

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ const app = express();
 // Middleware
 app.use(compression());
 app.use(
-    cors({
-      origin: [
-        "http://localhost:5173",
-        "https://prepstation-eight.vercel.app"
-      ]
-    })
-  );
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://prepstation-eight.vercel.app"
+    ]
+  })
+);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
